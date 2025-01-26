c0ac27fce188316f714db055cdf82419eb932b8a
Кирилл Атапович <katapovich08@gmail.com>
2025-01-26 19:17:31 +0300

    выполнена 7 задача 3 урока

---
+++ Task_03_07/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_03_07
+{
+    internal class Program
+    { 
+        /* Вывести таблицу скорости
+         * шаг=0,5 
+         *  v = g*t , 
+         * 2 g = 9,8 м/с2 –ускорение свободного падения
+        */
+        static void Main(string[] args)
+        {         
+                // заголовок таблицы
+
+                Console.WriteLine("Таблица скорости свободно падающего тела: ");
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("| t(c)    |    v(м/с) |");
+                Console.WriteLine("-----------------------");
+
+                // нахождение скорости по формуле и вывод значений
+
+                const double g = 9.8;
+
+                for (double t = 0; t <= 10; t += 0.5)
+                {
+                    double v = g * t;
+                    Console.WriteLine($"|  {t:F1}    |    {v:F2}   |");
+                    Console.WriteLine("-----------------------");
+                }
+            }
+    }
+}
