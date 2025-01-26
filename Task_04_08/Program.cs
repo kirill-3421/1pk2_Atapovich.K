308ec4067dd32431a04f2f6eb16a8f1c2431418d
Кирилл Атапович <katapovich08@gmail.com>
2025-01-26 23:57:05 +0300

    выполнена 8 задача 4 урока

---
+++ Task_04_08/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task_04_08
+{
+    internal class Program
+    {
+        /*
+         * Дан массив, содержащий последовательность 50 случайных чисел. 
+         * Найти количество пар элементов, значения  которых равны.
+        */
+        static void Main(string[] args)
+        {
+            //создание массива
+            int[] array = new int[50];
+            Random rand = new Random();
+
+            // заполнение массива случайными числами
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(1, 31);
+            }
+
+            // поиск равных пар чисел
+            int pairOfCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                        pairOfCount++;
+                }
+            }
+
+            // вывод количества пар
+            Console.WriteLine("Массив: " + string.Join(",", array));
+            Console.WriteLine($"Количество пар: {pairOfCount}");
+        }
+    }
+}
