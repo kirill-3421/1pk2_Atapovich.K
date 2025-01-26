f05da8062268d98909b738e805d57adfc45f445c
Кирилл Атапович <katapovich08@gmail.com>
2025-01-26 23:14:24 +0300

    выполнена 6 задача 4 урока

---
+++ Task_04_06/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_04_06
+{
+    internal class Program
+    {
+        /*
+         * Заполнить массив случайными положительными и отрицательными числами 
+         * все числа по модулю должны быть разными. 
+         * Это значит, что в массиве не может быть ни только двух равных чисел, но не может быть  двух равных по модулю. 
+         * В полученном массиве найти наибольшее по модулю число
+         */
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count;
+                do
+                {
+                    count = rand.Next(-100, 101);
+                } while (Array.Exists(array, x => Math.Abs(x) == Math.Abs(count)));
+                array[i] = count;
+            }
+
+            int maxModule = array[0];
+            foreach (var count in array)
+            {
+                if (Math.Abs(count) > Math.Abs(maxModule))
+                    maxModule = count;
+            }
+
+            Console.WriteLine("Массив: " + string.Join(", ", array));
+            Console.WriteLine("Наибольшее по модулю число: " + maxModule);
+        }
+    }
+}
