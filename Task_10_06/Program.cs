d1ad4272431e520276ffe38a29fbe4e9e0f85ce8
Кирилл Атапович <katapovich08@gmail.com>
2025-03-08 23:37:00 +0300

    выполнена 6 задача 10 урока

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    {
+        /*
+         * Создать Метод ArrayGeneration не возвращающий значения, принимает целое число n,
+         * выводит на консоль сгенерированный массив размерности n*n.
+         */
+
+        static void Main(string[] args)
+        {
+            int n = 6;
+            ArrayGeneration(n);
+        }
+
+        static void ArrayGeneration(int n)
+        {
+            int[,] count = new int[n, n];
+            Random rnd = new Random();
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    count[i, j] = rnd.Next(0, 101);
+                }
+            }
+
+            for(int i = 0;i < n; i++)
+            {
+                for (int j = 0;j < n; j++)
+                {
+                    Console.Write(count[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
