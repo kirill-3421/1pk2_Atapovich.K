29b6159e1c7cface83bb1d459958628c62dc0b88
Кирилл Атапович <katapovich08@gmail.com>
2025-03-19 18:18:04 +0300

    выполнена 3 задача 14 урока

---
+++ Task_14_03/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_03
+{
+    public class Math
+    {
+        //метод класса - вычисление факториала
+        public static long Factorial(int count)
+        {
+            if (count < 0)
+            {
+                return -1;
+            }
+
+            long result = 1;
+
+            for(int i = 2; i <= count; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
---
+++ Task_14_03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_14_03
+{
+    internal class Program
+    {
+        /*
+         Реализуйте статический метод Factorial, который принимает целое число и возвращает его факториал. Сделайте 
+         так, чтобы метод работал только для неотрицательных чисел
+         */
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите любое неотрицательное целое число: ");
+            int n = int.Parse(Console.ReadLine());
+
+            long factorial = Math.Factorial(n);
+
+            if (factorial == -1)
+            {
+                Console.WriteLine("Факториал вычисляется только для неотрицательных чисел.");
+            }
+            else
+            {
+                Console.WriteLine($"Факториал числа {n} равен - {factorial}.");
+            }
+        }
+    }
+}
