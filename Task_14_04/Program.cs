dedd379cd84c122b8eea3cf12037f24adbd9c55a
Кирилл Атапович <katapovich08@gmail.com>
2025-03-19 21:12:48 +0300

    выполнена 4 задача 14 урока

---
+++ Task_14_04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task_14_04
+{
+    internal class Program
+    {
+        /*
+         Определите класс User, который будет иметь статическое свойство CurrentUser, представляющее текущего 
+         пользователя, и метод для его установки.
+
+         */
+
+        static void Main(string[] args)
+        {
+            User user = new User { Name = "Кирилл", Email = "k.irill@gmail.com" };
+            User.SetCurrentUser(user);
+            Console.WriteLine($"Текущий пользователь - {User.CurrentUser.Name}\nEmail - {User.CurrentUser.Email}");
+        }
+    }
+}
---
+++ Task_14_04/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_04
+{
+    public class User
+    {
+        //свойство для хранения пользователя
+        public static User CurrentUser { get; set; }
+
+        //свойства для имени и почты пользователя
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        //метод класса - установка текущего пользователя
+        public static void SetCurrentUser(User user)
+        {
+            CurrentUser = user;
+        }
+    }
+}
