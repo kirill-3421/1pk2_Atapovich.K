1e6fa94e032a84d0823f0180bb74ca482315c6e3
Кирилл Атапович <katapovich08@gmail.com>
2025-03-19 17:28:09 +0300

    выполнена 2 задача 13 урока

---
+++ Task_13_02/Pet.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_02
+{
+    internal class Pet
+    {
+        //свойства класса
+        public string Name { get; set; }
+        public string View {  get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public bool IsHealth { get; set; }
+
+        //конструкторы класса
+        public Pet() { }
+        #region
+        public Pet(string name, string view, int age)
+        {
+
+            Name = name;
+            View = view;
+            Age = age;
+        }
+        #endregion
+
+        #region
+        public Pet(string name, string view, bool isHealth)
+        {
+            Name = name;
+            View = view;
+            IsHealth = isHealth;
+        }
+        #endregion
+
+        #region
+        public Pet(string name, string view, int age, int weight, bool isHealth)
+        {
+            Name = name;
+            View = view;
+            Age = age;
+            Weight = weight;
+            IsHealth = isHealth;
+        }
+        #endregion
+
+        //метод класса - вывод информации об объекте
+        public void GetInfo()
+        {
+            string n =  IsHealth ? "здоров" : "нездоров";
+            Console.WriteLine($"Кличка - {Name}\nВид - {View}\nВозраст - {Age} лет\nВес - {Weight} кг\nСостояние здоровья - {IsHealth}.");
+        }
+
+        //метод класса - изменение веса животного
+        public void ChangeWeight(int weight)
+        {
+            Weight = weight;
+            Console.WriteLine($"Новый вес питомца {Name} - {Weight} кг.");
+        }
+
+        //метод класса - изменение отметки состояния здоровья
+        public void ChangeHealth()
+        {
+            IsHealth = !IsHealth;
+            if (IsHealth == true)
+            {
+                Console.WriteLine("Здоров");
+            }
+            else
+            {
+                Console.WriteLine("Нездоров");
+            }
+        }
+    }
+}
---
+++ Task_13_02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_13_02
+{
+    internal class Program
+    {
+        /*
+         * создать класс питомца 
+           свойства:
+         * кличка, вид животного, возраст, вес, отметка о состонии здоровья (здоров/нездоров)
+           методы:
+         * вывод информации об объекте
+         * изменение значения веса животного
+         * изменение отметки о состоянии здоровья
+           конструторы:
+         * предусмотрите разные варианты инициализации объектов
+         */
+        static void Main(string[] args)
+        {
+            Pet pet = new Pet("Моня", "кошка", 5);
+            pet.Weight = 9;
+            pet.IsHealth = true;
+            pet.GetInfo();
+            pet.ChangeWeight(10);
+            pet.ChangeHealth();
+
+            Console.WriteLine("\n");
+
+            Pet pet2 = new Pet("Мухтар", "пёс", 7, 33, false);
+            pet2.GetInfo();
+            pet2.ChangeHealth();
+            pet2.ChangeWeight(52);
+        }
+    }
+}
