f2a7682ee78be2811df898c50883937eb85d1704
Кирилл Атапович <katapovich08@gmail.com>
2025-03-30 22:29:39 +0300

    выполнена 2 задача 19 урока

---
+++ Task_19_02/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task_19_02
+{
+    internal class Program
+    {
+        /*
+         Напишите программу, которая запрашивает у пользователя произвольный текст, содержащий предложения (есть знаки препинания).
+         программa после анализа выводит текст, разделенный на части:
+             a)По пробелам (отдельные слова построчно)
+             b)По предложениям (отдельные предложения построчно)
+              (используйте метод Split())
+
+         */
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст(ставьте знаки препинания, если они необходимы): ");
+            string input = Console.ReadLine();
+
+            //разделение текста по пробелам
+            string[] words = input.Split(' ');
+            Console.WriteLine("\nСлова в тексте:");
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrWhiteSpace(word)) // Проверка, что слово не пустое
+                {
+                    Console.WriteLine(word);
+                }
+            }
+
+            //разделение текста по предложениям
+            char[] sentenceDelimiters = { '.', '!', '?', '-', ';'};
+            string[] sentences = input.Split(sentenceDelimiters);
+
+            Console.WriteLine("\nПредложения в тексте:");
+            foreach (string sentence in sentences)
+            {
+                if (!string.IsNullOrWhiteSpace(sentence))
+                {
+                    Console.WriteLine(sentence.Trim()); //избавление от лишних пробелов
+                }
+            }
+        }
+    }
+}
