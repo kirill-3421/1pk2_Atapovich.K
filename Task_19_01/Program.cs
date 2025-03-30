6414006f8ec6794c43772258ed2785994e5263de
Кирилл Атапович <katapovich08@gmail.com>
2025-03-30 22:18:34 +0300

    выполнена 1 задача 19 урока

---
+++ Task_19_01/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_19_01
+{
+    internal class Program
+    {
+        /*
+        Напишите программу, в которой пользователь вводит произвольный текст в консоли, после чего программа запрашивает подстроку для поиска.
+        Если подстрока найдена - то программа запрашивает текст для того чтобы заменить на него эту подстроку 
+        (столько раз, сколько раз подстрока встречена в тексте)
+           Пример:
+            Введите строку: "Привет, мир!"
+            Введите подстроку для поиска: "мир"
+            Введите подстроку для замены: "друг"
+            Результат: "Привет, друг!"
+
+         */
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+            Console.Write("Введите подсроку для поиска: ");
+            string searchInput = Console.ReadLine();
+
+            if(input.Contains(searchInput))
+            {
+                Console.Write("Введите строку для замены: ");
+                string resultInput = Console.ReadLine();
+
+                string resultInput2 = input.Replace(searchInput, resultInput);
+                Console.WriteLine($"Результат: {resultInput2}");
+            }
+            else
+            {
+                Console.WriteLine("Подстрока не найдена, попробуйте снова");
+                return;
+            }
+        }
+    }
+}
