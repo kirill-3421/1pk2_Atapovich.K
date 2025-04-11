e214fa6a533c7524b6bdbd0bdec83fb3b49c5d86
Кирилл Атапович <katapovich08@gmail.com>
2025-04-12 00:04:51 +0300

    выполнена 7 задача 24 урока

---
+++ Task_24_07/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task_24_07
+{
+    internal class Program
+    {
+        /*
+          Реализуйте функцию, которая ищет заданное слово в текстовом файле и возвращает все строки, содержащие это 
+          слово (регистронезависимо)
+         */
+
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\docs\text.doc"; 
+            string searchWord = "хозяйство";
+            List<string> foundLines = FindLines(filePath, searchWord);
+
+            Console.WriteLine($"Строки, содержащие слово \"{searchWord}\": ");
+            foreach (string line in foundLines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static List<string> FindLines(string path, string word)
+        {
+            List<string> result = new List<string>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        result.Add(line);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
