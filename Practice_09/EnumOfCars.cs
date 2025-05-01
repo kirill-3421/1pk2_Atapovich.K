6f7eebf94dcafff42e71040aafddcb061d7368a1
Кирилл Атапович <katapovich08@gmail.com>
2025-05-01 10:46:44 +0300

    практика

---
+++ Practice_09/EnumOfCars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_09
+{
+    enum CarType
+    {
+        Economy = 1,
+        Comfort,
+        Business,
+        Premium
+    }
+}
